263c4b8c044574844ed04106de55a4cb8a6ae336
softenvarunpawar <vrnpwr001@gmail.com>
2020-05-11 21:29:31 +0530

    add services and services type -mrc

---
+++ app/Http/Controllers/ServicesTypeController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\ServicesType;
+use Illuminate\Http\Request;
+
+class ServicesTypeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\ServicesType  $servicesType
+     * @return \Illuminate\Http\Response
+     */
+    public function show(ServicesType $servicesType)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\ServicesType  $servicesType
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(ServicesType $servicesType)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\ServicesType  $servicesType
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, ServicesType $servicesType)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\ServicesType  $servicesType
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(ServicesType $servicesType)
+    {
+        //
+    }
+}
---
+++ database/migrations/2020_05_11_141913_create_services_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateServicesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('services', function (Blueprint $table) {
+            $table->id();
+            $table->integer('created_by')->nullable();
+            $table->integer('type_id');
+            $table->string('services');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('services');
+    }
+}
